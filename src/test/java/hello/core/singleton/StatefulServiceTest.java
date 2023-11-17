dbf043c1dac6f14d6ea448f2906a420d3fb32ec2
skn29 <skn2998@gmail.com>
2023-11-17 17:56:17 +0900

    변경

---
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -0,0 +1,23 @@
+package hello.core.singleton;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.Bean;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatefulServiceTest {
+
+    @Test
+    @DisplayName("d")
+    void statefulServiceSingleton(){
+
+    }
+
+    static class TestConfig{
+
+
+        @Bean
+
+    }
+}
