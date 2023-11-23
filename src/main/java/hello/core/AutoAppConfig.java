6e028a4aa9571d4e2a6405d44ce3c6e733168d18
jinbea10 <skn2998@gmail.com>
2023-11-23 22:01:13 +0900

    2023/11/23.commit

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration      //Configuration 안에 Component가 포함되고 있다
 @ComponentScan(    //Component 어노테이션이 붙은걸 자동으로 굵어와준다
-        basePackages = "hello.core.member", //어디서 부터 찾는지 지정, 여러개도 지정 가능, delfault는 1번쨰줄 패키지
+         //어디서 부터 찾는지 지정, 여러개도 지정 가능, delfault는 1번쨰줄 패키지, basePackages = "hello.core",
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
         //긁어와주는 것을 필터링 한다 (AppConfig 제외)
 )
---
+++ src/test/java/hello/core/autowired/AutowiredTest.java
@@ -0,0 +1,41 @@
+package hello.core.autowired;
+
+import hello.core.member.Member;
+import jakarta.annotation.Nullable;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.Optional;
+
+
+public class AutowiredTest {
+
+    @Test
+    void AutowiredOptional(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestBean.class);
+
+    }
+
+    static class TestBean {
+
+        @Autowired(required = false)        //호출이 않된다
+        public void setNoBean1(Member member) {
+            System.out.println("member = " + member);
+        }
+
+
+        @Autowired                          //null이 호출됌
+        public void setNoBean2(@Nullable Member member) {
+            System.out.println("member = " + member);
+        }
+
+        @Autowired                          //호출되면 Optoinal.empty가 호출됌
+        public void setNoBean3(Optional<Member> member) {
+            System.out.println("member = " + member);
+        }
+    }
+
+}
+
